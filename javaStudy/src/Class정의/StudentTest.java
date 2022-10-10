7fa183eb19c748df3b58db425e42ed467a2a12cc
z1hyun <wlgusdl4116@naver.com>
2022-10-10 10:47:03 +0900

    인스턴스 생성

--- javaStudy/src/Class정의/StudentTest.java
+++ javaStudy/src/Class정의/StudentTest.java
@@ -1,18 +1,17 @@
 package Class정의;
 
 public class StudentTest {
-	
-	public static void main(String[] args) {
-		
-		Student studentAhn = new Student(); // Student클래스 생성
-		studentAhn.studentName = "안승연";
-		
-		System.out.println(studentAhn.studentName);
-		System.out.println(studentAhn.getStudentName());
-	}
-	
-	/*
-	 * 클래스 이름이 같아도 패키지가 다르면 다른 클래스입니다.
-	 */
-	
+
+    public static void main(String[] args) {
+        
+        Student studentAhn = new Student(); // Student클래스 생성
+        studentAhn.studentName = "안승연";
+        
+        System.out.println(studentAhn.studentName);
+        System.out.println(studentAhn.getStudentName());
+    }
+    
+    /*
+     * 클래스 이름이 같아도 패키지가 다르면 다른 클래스입니다.
+     */
 }
