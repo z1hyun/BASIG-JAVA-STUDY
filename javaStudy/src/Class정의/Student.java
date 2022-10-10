5c2cb683034ed9bee0213ac41d00be6205d12eb8
z1hyun <wlgusdl4116@naver.com>
2022-10-10 10:28:09 +0900

    클래스에 메인메소드를 포함시키기

---
+++ javaStudy/src/Class정의/Student.java
@@ -0,0 +1,21 @@
+package Class정의;
+
+public class Student {
+	
+	int studentId;
+	String studentName;
+	int grade;
+	String address;
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public static void main(String[] args) {
+		Student studentAhn = new Student(); //Student 클래스 생성
+		studentAhn.studentName = "안연수";
+		
+		System.out.println(studentAhn.studentName);
+		System.out.println(studentAhn.getStudentName());
+	}
+}
