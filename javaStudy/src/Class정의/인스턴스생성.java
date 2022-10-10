fd77c5e813e2a0784b6f354a9a59dca04e2873ce
z1hyun <wlgusdl4116@naver.com>
2022-10-10 10:39:27 +0900

    인스턴스을 생성해보자

---
+++ javaStudy/src/Class정의/인스턴스생성.java
@@ -0,0 +1,19 @@
+package Class정의;
+
+public class 인스턴스생성 {
+
+	Student studentAhn = new Student();
+  //클래스형     변수 이름   = 생성자
+	
+	/*클래스가 생성된다는 것은 클래스를 실제 사용할 수 있는 메모리공간(힙 메모리)를 할당 받는다는 뜻
+	 * 이렇게 실제로 사용할 수 있도록 생성된 클래스를 인스턴스라하고
+	 * 인스턴스를 가리키는 클래스를 참조형 변수라한다.
+	 */
+	
+	/*
+	 * 위 코드 : Student 클래스 자료형으로 studentAhn변수를 선언하고
+	 * new Student()로 Student 클래스를 생성하여 studentAhn에 대입한다는 뜻
+	 * 이 때 studentAhn을 참조변수라한다.
+	 * 이 변수가 생성된 인스턴스를 가리킨다.
+	 */
+}
