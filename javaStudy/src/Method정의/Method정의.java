3c8c17d0037a3e601f4c6ce0ad6cadd206de6698
z1hyun <wlgusdl4116@naver.com>
2022-10-10 10:25:27 +0900

    Method의 정의

---
+++ javaStudy/src/Method정의/Method정의.java
@@ -0,0 +1,23 @@
+package Method정의;
+
+public class Method정의 {
+
+	/*
+	 * 메소드는 함수의 한 종류이다. 
+	 * 함수 : 하나이 기능을 수행하는 일련의 코드
+	 */
+	
+	/*
+	 * 함수를 정의해보자.
+	 */
+	int add(int num1, int num2) {
+		int result;
+		result = num1+num2;
+		return result;
+	}
+	/*
+	 * int반환형과 add라는 이름을 가진 함수.
+	 * 매개변수 = 함수내부에서 사용할 괄호 안의 변수를 매개변수라 한다.
+	 * return = (예약어) 반환값
+	 */
+}
